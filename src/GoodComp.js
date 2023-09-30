1a8138f7a2d04073f206eeb60ca54287e8b911f0
Harshda Mhase <harshadamhase12@gmail.com>
2023-09-29 23:07:00 -0400

    initial commit

---
+++ src/GoodComp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ImgPizza from './pizza.png'
+
+export default function GoodComp() {
+  return (
+    <div>
+      <img src={ImgPizza} alt='img'/>
+        </div>
+  )
+}
+
